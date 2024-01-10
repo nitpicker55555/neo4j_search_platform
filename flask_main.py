e988918f2b9668d700c5fc075d5f5344e77e3726
nitpicker55555 <1303385763@qq.com>
2024-01-11 00:45:22 +0100

    add upload function

--- flask_main.py
+++ flask_main.py
@@ -1,12 +1,14 @@
-from flask import Flask, request, render_template_string
+from flask import Flask, request, render_template_string,render_template
 import json
 from CreateDatabase import CreateDatabase
 app = Flask(__name__)
 instance_class=CreateDatabase()
 @app.route('/')
 def index():
-    return render_template_string(open('upload.html').read())  # Load the HTML file as a template
-
+    return render_template(('3dgraph.html'))  # Load the HTML file as a template
+@app.route('/second')
+def second():
+    return render_template('upload.html')
 @app.route('/submit', methods=['POST'])
 def submit():
     data = request.json  # Get JSON data sent from the front-end
